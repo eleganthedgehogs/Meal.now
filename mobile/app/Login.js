2774dda6b2a0301c822d121da47d71a1ee458cee
Vien Tang <vientang@gmail.com>
2016-11-01 16:26:56 -0700

    Blur background effect on log in page

--- mobile/app/Login.js
+++ mobile/app/Login.js
@@ -7,13 +7,13 @@ import {
   View,
   TouchableHighlight,
   Alert,
-  Dimensions,
+  Dimensions
 } from 'react-native';
 import t from 'tcomb-form-native';
 import MealList from './MealList';
 
 const width = Dimensions.get('window').width;
-
+const background = require('../assets/resized_background.jpg');
 const styles = StyleSheet.create({
   main: {
     flex: 1,
@@ -23,19 +23,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     // alignItems: 'center',
     alignSelf: 'center',
-    marginTop: 30,
+    marginTop: 100,
     width: width * 0.9,
     borderRadius: 5,
     flexDirection: 'column',
     // width: 150,
     // height: 300,
     padding: 20,
-    backgroundColor: 'rgba(255,255,255,.75)',
+    backgroundColor: 'rgba(255,255,255,.85)',
   },
   title: {
     fontSize: 50,
     fontFamily: 'Futura',
-    marginTop: 30,
+    marginTop: 50,
     backgroundColor: 'transparent',
     alignSelf: 'center',
     marginBottom: 30,
@@ -56,8 +56,8 @@ const styles = StyleSheet.create({
   },
   button: {
     height: 36,
-    backgroundColor: 'green',
-    borderColor: 'green',
+    backgroundColor: '#1E90FF',
+    borderColor: '#1E90FF',
     borderWidth: 1,
     borderRadius: 5,
     marginBottom: 10,
@@ -66,7 +66,9 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     flex: 1,
-  },
+    alignSelf: 'center',
+    backgroundColor: 'transparent',
+  }
 });
 // ENV Variables
 const loginUrl = 'https://mealdotnext4.herokuapp.com/api/user/authenticate';
@@ -159,16 +161,17 @@ export default class Login extends React.Component {
       <View style={styles.main}>
         <Image
           // if background image doesn't appear test url, site may have dropped uploaded image
-          source={{ uri: 'https://s21.postimg.org/azydn73pz/resized_background.jpg' }}
+          source={background}
           style={styles.backgroundImage}
-        >
+        >   
           <View style={styles.row}>
-            <Text style={styles.title}>Meal.next</Text>
+            <Text style={styles.title}>Meal.Next</Text>
           </View>
           <View style={styles.container}>
             <View style={styles.row}>
               <Form
                 ref="form"
+                style={{borderColor: 'red'}}
                 type={Person}
                 options={options}
               />
