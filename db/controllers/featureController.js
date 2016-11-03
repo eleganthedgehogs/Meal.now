db82aa37bf6d4e5dd4e0cdb1883270b1e5ab1c80
Joe <josephsanglee@gmail.com>
2016-11-03 13:23:14 -0700

    Integrate Clarifai API to grab tags for food; Begin integrating Google Places API to grab nearby restaurants based on location

--- db/controllers/featureController.js
+++ db/controllers/featureController.js
@@ -1,15 +1,7 @@
 const Promise = require('bluebird');
 const Feature = require('../models/featureModel');
-const rp = require('request-promise');
 const s3 = require('../../server/config/helpers.js');
-const Clarifai = require('clarifai');
-
-const app = new Clarifai.App(
-  'bnjpiSFcBcuwIAO5JkPxF60RePGyuEcJkMdV4orj',
-  'EmNsQ5M-cfGqELA28fFpSpcEoCfhgV0k5fG12hXT'
-);
-
-app.getToken();
+const utils = require('../../server/config/utils.js');
 
 // exports.location = (req, res) => {
 // // user sends the location to the server
@@ -28,17 +20,23 @@ exports.uploadImage = (req, res) => {
         return console.log('error uploading file:', err);
       }
 
-      var original = versions.filter(image => image.original)[0];
-      
-      //ISN'T RETURNING TAGS
-      app.models.predict('bd367be194cf45149e75f01d59f77ba7', original.url).then(
-        function(response) {
-          console.log(response);
-        },
-        function(err) {
-          console.log(err);
-        }
-      );
+      const original = versions.filter(image => image.original)[0];
+
+      // ISN'T RETURNING TAGS
+      utils.clarifai(original.url)
+      .then(tags => console.log(tags.classes))
+      .catch(error => console.log('Error getting tags in featureController', error));
     });
   }
+};
+
+exports.getNearbyPlaces = (req, res) => {
+  console.log('getting location');
+  const lat = req.body.coords.longitude;
+  const long = req.body.coords.longitude;
+
+  utils.getPlaces(lat, long)
+  .then(places => console.log(places))
+  .catch(error => console.log('Error getting places in featureController', error));
+  res.end();
 };
---
+++ server/config/secretExample.js
@@ -0,0 +1,4 @@
+exports.ACCESS_KEY_ID = 'accessKeyId';
+exports.SECRET_ACCESS_KEY = 'secretAccessKey';
+exports.CLARIFAI_TOKEN = 'clarifaiToken';
+exports.PLACES_KEY = 'placesKey';
