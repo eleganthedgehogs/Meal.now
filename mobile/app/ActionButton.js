3fbc071d90ce6ff3c50d1a60b9bb500b506a2519
Carlos Portillo <caportil@gmail.com>
2016-11-03 19:36:31 -0700

    Add Action Button component to import into separate views

---
+++ mobile/app/ActionButton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import ActionButton from 'react-native-action-button';
+import { Ionicons } from '@exponent/vector-icons';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  contentContainer: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingBottom: 60,
+  },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
+});
+
+const MainActionButton = () => {
+  return (
+      <ActionButton buttonColor="rgba(231,76,60,1)" position="right" offsetY={200}>
+        <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+          <Ionicons name="ios-search" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+      </ActionButton>
+  );
+}
+
+export default MainActionButton;
