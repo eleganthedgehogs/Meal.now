d4f62955ce76e3925a57f439e16bf41eaf93a71b
Carlos Portillo <caportil@gmail.com>
2016-11-05 15:48:36 -0700

    Add nutritional info view

--- mobile/app/MenuItem.js
+++ mobile/app/MenuItem.js
@@ -1,42 +1,97 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, View, ScrollView, Dimensions } from 'react-native';
 import Header from './Header';
-import { Container, Content, List, ListItem, Thumbnail, Title } from 'native-base';
+import { Container, Content, List, ListItem, Thumbnail, Title, Text } from 'native-base';
 import utils from '../Utils/utils';
+import Column from './Column';
+
+const width = Dimensions.get('window').width;
+
+
+const StandardizeInfo = (text) => {
+  // console.log('Standardize:', text, typeof text);
+  if (text) {
+    const Standardized = text.toString().replace('nf_', '').replace('_', ' ')
+    .replace('_', ' ').replace('_', ' ');
+    return Standardized[0].toUpperCase() + Standardized.slice(1);
+  }
+};
 
 class MenuItem extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props, 'props from pick MenuItem')
+    // console.log(props, 'props from pick MenuItem');
   }
 
   render() {
-    console.log('Nutrition info in MenuItem:', this.props.item);
+    // console.log('Nutrition info in MenuItem:', this.props.item);
     const NutritionInfo = JSON.parse(this.props.item.nutritionalInfo);
+    console.log('Full NutritionInfo:', NutritionInfo);
     return (
       <View style={styles.container}>
-        <Header />
+        <Header title={'Meal.info'}/>
         <ScrollView
-          contentContainerStyle={styles.conatiner}
+          contentContainerStyle={styles.container}
           showsVerticalScrollIndicator={false}
           alwaysBounceVertical
         >
-          <List>
-            <Title>{this.props.item.restaurantName}</Title>
-            <Title>{this.props.item.name}</Title>
-            {Object.keys(NutritionInfo).map((key) => (
-              <ListItem key={key} >
-                  <Text>{key}: {NutritionInfo[key]}</Text>
-              </ListItem>
-            ))}
-          </List> 
+
+
+
+          <Title>{this.props.item.restaurantName}</Title>
+
+          <Title>{this.props.item.name}</Title>
+          {Object.keys(NutritionInfo).map((key) => {
+            if (key.toString().indexOf('_id') === -1 && key.toString().indexOf('updated') === -1 && key.toString().indexOf('item') === -1 && key.toString().indexOf('brand') === -1 && NutritionInfo[key]) {
+              const standardizedKey = StandardizeInfo(key);
+              console.log('Z', standardizedKey, NutritionInfo[key]);
+              return (
+                <ListItem style={styles2.table} key={key} >
+                    <Text  style={styles2.text} >{standardizedKey}: {NutritionInfo[key]}</Text>
+                </ListItem>
+                )
+            } else {
+              return null;
+            }
+          })}
+
+
         </ScrollView>
+
       </View>
     );
   }
 }
 
 export default MenuItem;
+
+/*
+
+<Column
+  data='QtyData'//{compileNutrition(props.recipe.digest)}
+  name="Qty"
+  index="totalUnit"
+  alignRight
+/>
+<Column
+  data='dailyPercentData'//{compileNutrition(props.recipe.digest)}
+  name="Daily"
+  index="dailyPercent"
+  alignRight
+/>
+
+<List>
+  <Title>{this.props.item.restaurantName}</Title>
+  <Title>{this.props.item.name}</Title>
+  {Object.keys(NutritionInfo).map((key) => (
+    <ListItem key={key} >
+        <Text>{key}: {NutritionInfo[key]}</Text>
+    </ListItem>
+  ))}
+</List> 
+
+*/
+
 
 /************************ STYLES *************************/
 
@@ -56,4 +111,44 @@ const styles = StyleSheet.create({
     top: 100,
     right: 25
   }
+});
+
+/************************ STYLES *************************/
+
+const styles2 = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    width: width * 0.9,
+    marginTop: 10,
+  },
+  container: {
+    alignItems: 'center',
+    flex: 1,
+  },
+  picture: {
+    width: width * 0.9,
+    height: 250,
+    opacity: 1,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  click: { fontSize: 30 },
+  table: {
+    width: width * 0.9,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderWidth: 2,
+    borderColor: 'black',
+    borderRadius: 5,
+    padding: 5,
+    marginTop: 10,
+  },
+  scroller: {
+    marginBottom: 50,
+  },
+  text: {
+    fontFamily: 'Futura',
+  },
 });
